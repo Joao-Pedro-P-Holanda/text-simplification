d99297622c13ca0a8e4fe277d6dbdf7e52613935
Joao-Pedro-P-Holanda <joaopedroph.dev@gmail.com>
2025-05-17 21:58:49 -0300

    feat: add UFC Inova notice scrapper
    Add web scrapper for UFC Inova that extract non-simplified pdfs from a
    given year range

---
+++ scrape_notices_ufc_inova.py
@@ -0,0 +1,71 @@
+"""
+This module scrape the complete version of UFC Inova notices
+"""
+
+import asyncio
+import re
+from bs4 import BeautifulSoup
+import os
+import httpx
+from urllib.parse import urljoin
+
+
+def _id_for_file(count: int, year: int):
+    return f"{year}_ufc_inova_{count}"
+
+
+async def main():
+    base_path = "https://ufcinova.ufc.br/pt/"
+
+    years = range(2020, 2026)
+
+    for year in years:
+        notices_page_html = httpx.get(
+            urljoin(base_path, str(year) + "-2"), follow_redirects=True
+        ).text
+        soup = BeautifulSoup(notices_page_html, features="html.parser")
+
+        links = _selection_for_year(soup, year)
+
+        for i, link in enumerate(links):
+            response = httpx.get(link, timeout=30)
+            os.makedirs("./data/complete", exist_ok=True)
+            with open(f"./data/complete/{_id_for_file(i + 1, year)}.pdf", "wb") as f:
+                f.write(response.content)
+
+
+def _selection_for_year(soup: BeautifulSoup, year: int) -> list[str]:
+    notices_pattern = re.compile(r"Edital", re.IGNORECASE)
+    exclude_patterns = [
+        re.compile(
+            r"simplificad",
+            re.IGNORECASE,
+        ),
+        re.compile(r"simples", re.IGNORECASE),
+        re.compile(r"retificad", re.IGNORECASE),
+    ]
+
+    links = []
+    match year:
+        case 2025:
+            exceptions = [
+                "https://prointer.ufc.br/wp-content/uploads/2025/01/edital-001-2025-hackathon-tre-ufc-publicado.pdf"
+            ]
+
+        case _:
+            exceptions = []
+
+    a_tags = soup.find_all(
+        "a",
+        string=lambda text: text
+        and notices_pattern.search(text)
+        and not any([pattern.search(text) for pattern in exclude_patterns]),
+    )
+
+    links.extend(a["href"] for a in a_tags)
+
+    return [link for link in links if link not in exceptions]
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
