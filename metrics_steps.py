2dd59d2a248927a3959e75567f35af8400dcb07f
Joao-Pedro-P-Holanda <joaopedroph.dev@gmail.com>
2025-05-31 22:17:52 -0300

    wip: add svo ratio, passive_ratio and long_sentence_ratio

--- metrics_steps.py
+++ metrics_steps.py
@@ -10,6 +10,8 @@ from urllib.parse import urljoin
 import spacy
 from gloe import partial_transformer, transformer
 from spacy.matcher import Matcher
+from spacy.tokens.doc import Doc
+from spacy.tokens.span import Span
 
 from schema import (
     Document,
@@ -74,7 +76,7 @@ def extract_document_statistics(
 
 
 @transformer
-def extract_nilc_metrix(document: Document):
+def extract_min_nilc_metrix(document: Document):
     metrics_url = urljoin(
         str(config["nilc_metrix_url"]), "/api/v1/metrix/_min/yyy?format=json"
     )
@@ -90,6 +92,95 @@ def extract_nilc_metrix(document: Document):
     response_dict.update(document.model_dump())
 
     return NILCMetrics.model_validate(response_dict)
+
+
+@partial_transformer
+def extract_syntatic_nilc_metrix(
+    document: Document,
+    nlp_loader: Callable[[str], spacy.language.Language],
+    model_name: str,
+):
+    """
+    Extracts another subset of nilc metrix that depends on a licensed parser with spacy
+    instead
+    """
+
+    nlp = nlp_loader(model_name)
+
+    matcher = Matcher(nlp.vocab)
+
+    spacy_doc = nlp(document.text)
+
+    sentences = list(spacy_doc.sents)
+
+    _passive_ratio(spacy_doc, len(sentences), matcher)
+
+    _non_svo(spacy_doc)
+
+    print(_long_sentence_ratio(sentences))
+
+
+def _non_svo(doc: Doc) -> float:
+    """
+    Finds all sentences that are out of SVO order
+    Based on the implementation in
+    https://github.com/NSchrading/intro-spacy-nlp/blob/master/subject_object_extraction.py
+    """
+
+    non_svo = []
+
+    subject_deps = ["nsubj", "nsubjpass", "csubj", "csubjpass", "agent", "expl"]
+    object_deps = ["dobj", "dative", "attr", "oprd"]
+
+    verbs = [token for token in doc if token.pos_ == "VERB" and token.dep_ != "aux"]
+
+    for verb in verbs:
+        # finds the position of the entities
+        # if a subject comes after or the object comes before the sentences is not svo
+        for child in verb.children:
+            if child.dep_ in subject_deps and child.dep > verb.i:
+                non_svo.append(verb.sent)
+            elif child.dep_ in object_deps and child.i < verb.i:
+                non_svo.append(verb.sent)
+
+    return len(non_svo) / len(list(doc.sents))
+
+
+def _passive_ratio(doc, num_of_sents: int, matcher: Matcher):
+    passive_rule = [{"DEP": "nsubjpass"}]
+    matcher.add("Passive voice", [passive_rule])
+
+    result = len(matcher(doc)) / num_of_sents
+
+    return result
+
+
+def _words_before_main_verb(sents: list[Span]) -> float: ...
+
+
+def _long_sentence_ratio(sents: list[Span]) -> float:
+    """
+    Proportion of sentences with more than 15 words
+    """
+    i = 0
+    for sent in sents:
+        tokens = [token.text.lower() for token in sent if token.is_alpha]
+        if len(tokens) > 15:
+            i += 1
+
+    return i / len(sents)
+
+
+def _aux_plus_PCP_per_sentence(sents: list[Span]) -> float: ...
+
+
+def _coreference_pronoun_ratio(sents: list[Span]) -> float: ...
+
+
+def _demonstrative_pronoun_ratio(sents: list[Span]) -> float: ...
+
+
+def _foreign_word_ratio(sents: list[Span]) -> float: ...
 
 
 @transformer
